66a394f015a11b1c0f8678d2b0c4b29c8b1d8536
Priscila Alves <cilaandrade67@gmail.com>
2023-01-04 07:33:37 -0300

    obter primeiros caracteres da string

---
+++ JS Fundamentals/obter-primeiros-caracteres-string.js
@@ -0,0 +1,10 @@
+/* Exercício para treinar JavaScript, conhecendo conceitos básicos: Função obter primeiros caracteres da string*/ 
+
+
+function myFunction(a) {
+
+return a.slice(0, 3);
+
+}
+
+myFunction(javaScript)
