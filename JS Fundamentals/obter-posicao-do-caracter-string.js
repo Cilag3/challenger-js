200d45c26396434ad6110b37cee0c4ea9c3ea314
Priscila Alves <cilaandrade67@gmail.com>
2023-01-04 07:43:35 -0300

    obter posição de caracteres na string

---
+++ JS Fundamentals/obter-posicao-do-caracter-string.js
@@ -0,0 +1,10 @@
+/* Exercício para treinar JavaScript, conhecendo conceitos básicos: Função para obter posição de caracteres na string*/
+
+
+function myFunction(a) {
+
+return a.indexOf('is');
+
+}
+
+myFunction(Paris)
