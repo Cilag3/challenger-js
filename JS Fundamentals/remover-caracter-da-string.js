8c6da3afba42feeb17ae3075642f72f2e66f2c8b
Priscila Alves <cilaandrade67@gmail.com>
2023-01-04 07:06:57 -0300

    remover os 3 primeiros caracteries da string

---
+++ JS Fundamentals/remover-caracter-da-string.js
@@ -0,0 +1,8 @@
+/* Exercício para treinar JavaScript, conhecendo conceitos básicos: Função para remover os 3 primeiros caracteries da string*/ 
+
+
+function myFunction (a){
+
+return a.slice(3);
+
+}
