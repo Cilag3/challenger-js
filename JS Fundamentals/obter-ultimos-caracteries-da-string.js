11bc4148ae195ed2ffe3f23ffead2fa693a10182
Priscila Alves <cilaandrade67@gmail.com>
2023-01-04 07:17:07 -0300

    obter ultimos caracteries da string

---
+++ JS Fundamentals/obter-ultimos-caracteries-da-string.js
@@ -0,0 +1,10 @@
+/* Exercício para treinar JavaScript, conhecendo conceitos básicos: Função para obter ultimos caracteries da string*/ 
+
+
+function myFunction (str) {
+
+return str.slice(str.length -3);
+
+}
+
+myFunction('JavaScript')
