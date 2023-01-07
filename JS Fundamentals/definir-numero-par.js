cc37c6d1432587021be0ab71a63d40ccce060dfc
Priscila Alves <cilaandrade67@gmail.com>
2023-01-07 16:13:54 -0300

    descobrir se o númwro é par

---
+++ JS Fundamentals/definir-numero-par.js
@@ -0,0 +1,10 @@
+
+/*Escreva uma função que receba um número como argumento. Se o número for par, retorna verdadeiro. Caso contrário, retorne falso*/ 
+
+function myFunction(a){
+
+return a % 2 === 0;
+
+}
+
+myFunction('String')
