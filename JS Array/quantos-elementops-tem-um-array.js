4c0b88e6cb2b8519d02beba8aca88f3f973545b2
Priscila Alves <cilaandrade67@gmail.com>
2023-01-13 11:21:43 -0300

    quantos elementos tem um array

---
+++ JS Array/quantos-elementops-tem-um-array.js
@@ -0,0 +1,18 @@
+/*Escreva uma função que receba um array (a) como argumento. Retorna o número de elementos em a.*/
+
+
+function myFunction(a) {
+
+return a.length;
+
+}
+
+myFunction([1,2,2,4])
+/*Esperado
+4*/
+myFunction([9,9,9])
+/*Esperado
+3*/
+myFunction([4,3,2,1,0])
+/*Esperado
+5*/
